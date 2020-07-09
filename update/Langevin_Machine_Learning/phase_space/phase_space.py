40339f6e8cd41f71be9a4cc564ed7e60b5f3c5dc
spark <ssojeongpark@gmail.com>
2020-07-09 17:07:45 +0800

    deepcopy phase space

--- update/Langevin_Machine_Learning/phase_space/phase_space.py
+++ update/Langevin_Machine_Learning/phase_space/phase_space.py
@@ -7,6 +7,7 @@ Created on Thu Jun 11 10:21:28 2020
 """
 
 import numpy as np
+import copy
 
 class phase_space :
     '''phase space container class that have a 
@@ -21,19 +22,19 @@ class phase_space :
 
     def set_p(self, p_list):
         print('phase_space.py p_list', p_list.shape)
-        self._p_list = p_list 
+        self._p_list = copy.deepcopy(p_list)
     
     def set_q(self, q_list):
         print('phase_space.py q_list',q_list.shape)
-        self._q_list = q_list 
+        self._q_list = copy.deepcopy(q_list)
     
     def get_p(self):
         print('phase_space.py _p_list', self._p_list.shape)
-        return self._p_list # N X particle X DIM array
+        return copy.deepcopy(self._p_list) # N X particle X DIM array
     
     def get_q(self):
         print('phase_space.py _q_list', self._q_list.shape)
-        return self._q_list # N X particle X DIM array
+        return copy.deepcopy(self._q_list) # N X particle X DIM array
 
     def read(self, filename, samples = -1):
         '''function to read the phase space file, 
@@ -52,6 +53,7 @@ class phase_space :
         self._q_list = np.array(phase_space[:samples]) # cast to numpy just in case its pickled obj
         print(self._q_list.shape)
         self._p_list = np.array(self._q_list[:samples])
+        print(self._p_list.shape)
         
         try : 
             assert self._q_list.shape == self._p_list.shape
