2613306485afa3e163b132a41740c66ee26d7300
spark <ssojeongpark@gmail.com>
2021-01-27 06:09:01 +0800

    remove grid list

--- HNNwLJ/phase_space/phase_space.py
+++ HNNwLJ/phase_space/phase_space.py
@@ -13,7 +13,6 @@ class phase_space :
         '''initialize phase space container of N X particle X DIM dimension'''
         self._q_list = None
         self._p_list = None
-        self._grid_list = None
 
     def set_p(self, p_list):
         # self._p_list = copy.deepcopy(p_list)
@@ -23,10 +22,6 @@ class phase_space :
         # self._q_list = copy.deepcopy(q_list)
         self._q_list = q_list.clone()
 
-    def set_grid(self, grid_list):
-        # self._q_list = copy.deepcopy(q_list)
-        self._grid_list = grid_list.clone()
-    
     def get_p(self):
         # return copy.deepcopy(self._p_list) # nsamples N X particle X DIM array
         return self._p_list.clone()
@@ -35,24 +30,10 @@ class phase_space :
         # return copy.deepcopy(self._q_list) # nsamples N X particle X DIM array
         return self._q_list.clone()
 
-    def get_grid(self):
-        # return copy.deepcopy(self._q_list) # nsamples N X particle X DIM array
-        return self._grid_list.clone()
-
     def read(self, filename, nsamples):
-        '''function to read the phase space file, 
-        the phase space numpy is arranged in q_list ,p_list 
-        
-        Parameters
-        ----------
-        filename : str 
-            file to be read for phase space
-        nsamples : int
-            nsamples per file , default everything (-1)
-        '''
-        # phase_space = torch.from_numpy(np.load(filename))
+
         phase_space = torch.load(filename)
-        # print(phase_space.shape)
+
         self._q_list =  phase_space[0][:nsamples]
         self._p_list =  phase_space[1][:nsamples]
 
@@ -60,6 +41,5 @@ class phase_space :
             assert self._q_list.shape == self._p_list.shape
         except : 
             raise Exception('does not have shape method or shape differs')
-         # assert they have the same 
 
         return self._q_list, self._p_list
