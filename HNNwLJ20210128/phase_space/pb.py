12843c070d3aa9a444a5371d39b46609f17a9bc9
spark <ssojeongpark@gmail.com>
2021-02-01 16:38:02 +0800

    parent class

--- HNNwLJ20210128/phase_space/pb.py
+++ HNNwLJ20210128/phase_space/pb.py
@@ -8,16 +8,24 @@ class pb:
     def adjust_real(self, q, boxsize): #use in verlet and other classes
 
          indices = torch.where(torch.abs(q)>0.5*boxsize)
+         shift = torch.round(q[indices] / boxsize) * boxsize
+         # print('q i ',q[indices])
+         # print('shift ',shift)
          q[indices] = q[indices] - torch.round(q[indices] / boxsize) * boxsize
 
-    def debug_pbc(self,q,boxsize):
+    def debug_pbc(self, q, boxsize):
 
-        bool = torch.abs(q) > 0.5*boxsize
-        #print(bool.any())
+        bool = torch.abs(q) > 0.5 * boxsize
+        # print('debug_pbc', bool.any())
+
         if bool.any() == True: # if values have any above condition, return true.
-            index = torch.where(torch.abs(q)>0.5*boxsize)
+            #print('q', q)
+            index = torch.where(torch.abs(q) > 0.5 * boxsize)
             debug = q[index]
+            print('q[185]',q[185])
+            print('index', index)
             print('debug_pbc',debug)
+
             raise ValueError('pbc not applied')
 
     def debug_pbc_reduced(self,q):
@@ -52,10 +60,25 @@ class pb:
         indices = torch.where(torch.abs(dq)>0.5)
         dq[indices] = dq[indices] - torch.round(dq[indices])
         # print('pbc',dq)
-        dq = dq[dq.nonzero(as_tuple=True)].reshape(nparticle, nparticle - 1, DIM)
-        # print(dq)
-        dd = torch.sqrt(torch.sum(dq*dq,dim=2))
+        dq_reduce_matrix = torch.zeros( (nparticle, (nparticle - 1), DIM) )
+
+        for i in range(nparticle):
+            x = 0  # all index case i=j and i != j
+            for j in range(nparticle):
+                if i != j:
+                    # print(i,j)
+                    # print(delta_init_q_[i,j,:])
+                    dq_reduce_matrix[i][x] = dq[i, j, :]
+
+                    x = x + 1
+
+        # print(dq_reduce_matrix)
+
+        # previous method - wrong
+        # dq = dq[dq.nonzero(as_tuple=True)].reshape(nparticle, nparticle - 1, DIM)
+
+        dd = torch.sqrt(torch.sum(dq_reduce_matrix * dq_reduce_matrix, dim=2))
         # print(dd)
 
-        return dq, dd
+        return dq_reduce_matrix, dd
 
