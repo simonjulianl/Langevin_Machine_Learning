f418278e7bfa63cab10dddadf1929d57b9cb0099
spark <ssojeongpark@gmail.com>
2021-02-01 16:32:51 +0800

    set parameters

--- HNNwLJ20210128/parameters/MC_paramaters.py
+++ HNNwLJ20210128/parameters/MC_paramaters.py
@@ -5,13 +5,13 @@ class MC_parameters:
     seed = 23645
     kB = 1.0
     temperature = 0.04
-    nsamples = 5
-    nparticle = 2
+    nsamples = 500
+    nparticle = 4
     DIM = 2
     mass = 1
     rho = 0.1
     boxsize = math.sqrt(nparticle / rho)
     interval = 40 # take mc step every interval
-    iterations = 2000
+    iterations = 60000
     DISCARD = iterations - (nsamples * interval)
     dq = 0.1
--- HNNwLJ20210128/parameters/MD_paramaters.py
+++ HNNwLJ20210128/parameters/MD_paramaters.py
@@ -4,14 +4,14 @@ import math
 class MD_parameters:
 
     seed = 42657933
-    nsamples = 400
-    select_nsamples = 400
+    nsamples = 500
+    select_nsamples = 40
     nsamples_ML = 1
     nparticle = 4
     DIM = 2
     mass = 1
     rho = 0.1
     boxsize = math.sqrt(nparticle / rho)
-    tau_short = 0.0001  # short time step for label
+    tau_short = 0.001  # short time step for label
     tau_long = 0.1
     integrator_method = methods.linear_velocity_verlet
