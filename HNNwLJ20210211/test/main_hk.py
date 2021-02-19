5d0fe0dc513f90727f0d7cbdeaef644b420849d5
ssojeong <62988783+ssojeong@users.noreply.github.com>
2021-02-19 21:17:46 +0800

    Update main_hk.py

--- HNNwLJ20210211/test/main_hk.py
+++ HNNwLJ20210211/test/main_hk.py
@@ -30,7 +30,7 @@ def get_indices(s):
 #        if mr[i,0] == 1:
 #            ind.append(i)
 #    return ind  
-    ind = torch.nonzero(m)
+    ind = m.nonzero(as_tuple=True)
     return ind
 
 # ============================================
