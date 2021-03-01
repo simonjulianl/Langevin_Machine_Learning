b845bc61383443caac5e1cdcba7257d7e7746834
spark <ssojeongpark@gmail.com>
2021-03-01 18:36:11 +0800

    not correct index

--- HNNwLJ20210224/test/k_nearest_index_test.py
+++ HNNwLJ20210224/test/k_nearest_index_test.py
@@ -24,12 +24,23 @@ def show_grid_nparticles(grid_list, q_list):
 
 if __name__ == '__main__':
 
+    seed = 9372211
+    torch.manual_seed(seed)
+    # torch.backends.cudnn.deterministic = True  # Processing speed may be lower then when the models functions nondeterministically.
+    # torch.backends.cudnn.benchmark = False
+    torch.cuda.manual_seed_all(seed)
+
     nparticle = 2
     npixels = 10
-    boxsize = 2
+    boxsize = 2.
     DIM = 2
     grid_interval = boxsize / npixels
+
     grid = build_gridpoint(npixels, boxsize, DIM)
+    # print('grid', grid)
+    U = torch.rand(npixels, npixels)
+    # print('U', U)
+    # print(U.shape)
 
     # q_list = [[-0.5, -0.5],[0.5, 0.5]]
     q_list = [[-0.95, -0.95],[0.5, 0.5]]
@@ -37,8 +48,13 @@ if __name__ == '__main__':
     _q_list = torch.tensor(q_list, dtype=torch.float64)
     print('q_list', _q_list)
     # show_grid_nparticles(grid, _q_list)
+
+    force4cnn = torch.zeros((nparticle, DIM))
+
     for i in range(nparticle):
+
         print(i)
+
         k_nearest_up_left = torch.floor(_q_list[i] / grid_interval ) * grid_interval
         k_nearest_up_left_x = k_nearest_up_left[0]
         k_nearest_up_left_y = k_nearest_up_left[1]
@@ -47,22 +63,26 @@ if __name__ == '__main__':
         k_nearest_down_left = torch.tensor([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y])
         k_nearest_down_right = torch.tensor([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y + grid_interval])
 
-        print(k_nearest_up_left)
-        print(k_nearest_up_right)
-        print(k_nearest_down_left)
-        print(k_nearest_down_right)
+        # print(k_nearest_up_left)
+        # print(k_nearest_up_right)
+        # print(k_nearest_down_left)
+        # print(k_nearest_down_right)
 
-        up_left_coord = ( boxsize / 2 + k_nearest_up_left ) / grid_interval
-        up_left_index = up_left_coord[0] * npixels + up_left_coord[1]
+        k_nearest = torch.stack([k_nearest_up_left, k_nearest_up_right, k_nearest_down_left, k_nearest_down_right])
+        print(k_nearest)
 
-        up_right_coord = (boxsize / 2 + k_nearest_up_right) / grid_interval
-        up_right_index = up_right_coord[0] * npixels + up_right_coord[1]
+        k_nearest_coord = ( boxsize / 2. + k_nearest ) / grid_interval
+        print(k_nearest_coord)
+        print(k_nearest_coord.long())
 
-        down_left_coord = (boxsize / 2 + k_nearest_down_left) / grid_interval
-        down_left_index = down_left_coord[0] * npixels + down_left_coord[1]
+        up_left_index = k_nearest_coord[:,0] * npixels + k_nearest_coord[:,1]
+        print(up_left_index)
+        print(up_left_index.type(torch.long))
+        quit()
 
-        down_right_coord = (boxsize / 2 + k_nearest_down_right) / grid_interval
-        down_right_index = down_right_coord[0] * npixels + down_right_coord[1]
 
-        print(up_left_coord, up_right_coord, down_left_coord, down_right_coord)
-        print(up_left_index, up_right_index, down_left_index, down_right_index)
+        print(up_left_coord.type(torch.long), up_right_coord.type(torch.long), down_left_coord.type(torch.long), down_right_coord.type(torch.long))
+        print(up_left_coord.dtype, up_right_coord.dtype, down_left_coord.dtype, down_right_coord.dtype)
+
+        # print(up_left_coord, up_right_coord, down_left_coord, down_right_coord)
+        # print(U[up_left_coord[0],up_left_coord[1]], U[up_right_coord[0],up_right_coord[1]], U[down_left_coord[0],down_left_coord[1]], U[down_right_coord[0],down_right_coord[1]])
