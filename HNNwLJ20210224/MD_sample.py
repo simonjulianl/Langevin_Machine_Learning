b48d544fc63b4ba4c9e9f80c93007308503a8780
spark <ssojeongpark@gmail.com>
2021-03-04 05:54:24 +0800

    MD sim

--- HNNwLJ20210224/MD_sample.py
+++ HNNwLJ20210224/MD_sample.py
@@ -15,14 +15,16 @@ import torch
 
 nsamples = MD_parameters.nsamples
 nparticle = MC_parameters.nparticle
+temp = MC_parameters.temperature
 tau_long = MD_parameters.tau_long
 tau_short = MD_parameters.tau_short
 nsamples_cur = nsamples
-tau_cur = tau_long
-MD_iterations = int(tau_long / tau_short)
+tau_cur = tau_short # find gold standard
+pair_interval = int(tau_long / tau_short)
+MD_iterations = int(MD_parameters.max_ts / tau_short)
 
-print('nparticle tau_long tau_short  nsamples_batch')
-print(nparticle, tau_long, tau_short, MD_parameters.nsamples_batch)
+print('nparticle tau pair_interval MD_iterations')
+print(nparticle, tau_short, pair_interval, MD_iterations)
 
 seed = 9372211
 torch.manual_seed(seed)
@@ -45,11 +47,22 @@ init_path = base_dir + '/init_config/'
 
 data_io_obj = data_io(init_path)
 init_q, init_p = data_io_obj.loadq_p('valid')
-print(init_q, init_p)
+
 phase_space.set_q(init_q)
 phase_space.set_p(init_p)
 
-q_hist, p_hist = linear_integrator_obj.step( noMLhamiltonian, phase_space, MD_iterations, nsamples_cur, tau_cur)
-print(q_hist[-1])
-print(p_hist[-1])
+q_list, p_list = linear_integrator_obj.step( noMLhamiltonian, phase_space, MD_iterations, nsamples_cur, tau_cur)
 
+init_q = torch.unsqueeze(init_q, dim=0)
+init_p = torch.unsqueeze(init_p, dim=0)
+
+q_hist_ = torch.cat((init_q, q_list.double()), dim=0)
+p_hist_ = torch.cat((init_p, p_list.double()), dim=0)
+
+# pair w large time step
+q_hist = q_hist_[0::pair_interval]
+p_hist = p_hist_[0::pair_interval]
+
+base_library = os.path.abspath('gold_standard')
+
+torch.save(phase_space, base_library + '/nsample{}_T{}_ts{}_iter{}_vv_{}sampled.pt'.format(nparticle,temp,tau_short,MD_iterations,nsamples))
