7d74456e58dadd956d0edc105047e13adf4448e1
spark <ssojeongpark@gmail.com>
2021-03-05 13:38:09 +0800

    calc predicted fields each particle

--- HNNwLJ20210224/test/k_nearest_index_test.py
+++ HNNwLJ20210224/test/k_nearest_index_test.py
@@ -2,6 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 
 def build_gridpoint(npixels, boxsize, DIM):
+
     xvalues = torch.arange(0, npixels, dtype=torch.float64)
     xvalues = xvalues - 0.5 * npixels
     yvalues = torch.arange(0, npixels, dtype=torch.float64)
@@ -22,6 +23,39 @@ def show_grid_nparticles(grid_list, q_list):
         plt.show()
         plt.close()
 
+def euclidean_distance(vector1, vector2):
+    # print('distance', vector1, vector2)
+    # print('distance', vector1.shape, vector2.shape)
+    return torch.sqrt(torch.sum(torch.pow(vector1 - vector2, 2), dim=1))  # sum DIM that is dim=1 <- nparticle x DIM
+
+def k_nearest_grids( q_list, grid_interval):
+    # to calculate distance between particle and grid
+    print('def k nearest grids')
+    print(q_list)
+    k_nearest_up_left = torch.floor( q_list / grid_interval) * grid_interval
+
+    k_nearest_up_left_x = k_nearest_up_left[:, 0]
+    k_nearest_up_left_x = k_nearest_up_left_x.reshape(-1, 1)
+    k_nearest_up_left_y = k_nearest_up_left[:, 1]
+    k_nearest_up_left_y = k_nearest_up_left_y.reshape(-1, 1)
+
+    k_nearest_up_right = torch.cat([k_nearest_up_left_x, k_nearest_up_left_y + grid_interval], dim=1)
+    k_nearest_down_left = torch.cat([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y], dim=1)
+    k_nearest_down_right = torch.cat([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y + grid_interval], dim=1)
+
+    print(k_nearest_up_left, k_nearest_up_right, k_nearest_down_left, k_nearest_down_right)
+    print(k_nearest_up_left.shape, k_nearest_up_right.shape, k_nearest_down_left.shape, k_nearest_down_right.shape)
+    k_nearest = torch.stack([k_nearest_up_left, k_nearest_up_right, k_nearest_down_left, k_nearest_down_right], dim=1)  # 4_nearest x samples x DIM
+
+    return k_nearest
+
+def k_nearest_coord(k_nearest, grid_interval):
+    # to find force k neareset grids
+
+    k_nearest_coord = (boxsize / 2. + k_nearest) / grid_interval
+    kind = torch.round(k_nearest_coord).long()  # 4_nearest x nsamples x DIM
+    return kind
+
 if __name__ == '__main__':
 
     seed = 9372211
@@ -30,43 +64,108 @@ if __name__ == '__main__':
     # torch.backends.cudnn.benchmark = False
     torch.cuda.manual_seed_all(seed)
 
+    nsamples = 2
     nparticle = 2
     npixels = 10
     boxsize = 2.
     DIM = 2
     grid_interval = boxsize / npixels
+    print('grid interval', grid_interval)
 
     grid = build_gridpoint(npixels, boxsize, DIM)
     # print('grid', grid)
-    U = torch.rand(npixels, npixels)
+    U1 = torch.rand(2, npixels, npixels)
+    # # print(U1)
+    U2 = torch.rand(2, npixels, npixels)
+    # # print(U2)
+    U = torch.stack([U1, U2], dim=0)
+    # print('U', U)
+    # print(U.shape)
+
+    # U = torch.rand(1, 2, npixels, npixels)
     # print('U', U)
     # print(U.shape)
 
     # q_list = [[-0.5, -0.5],[0.5, 0.5]]
-    q_list = [[-0.95, -0.95],[0.5, 0.5]]
+    q_list = [[[-0.95, -0.95],[0.5, 0.5]], [[-0.5, -0.5],[0.5, 0.5]]]
+    # q_list = [[[-0.95, -0.95],[0.5, 0.5]]]
 
     _q_list = torch.tensor(q_list, dtype=torch.float64)
-    print('q_list', _q_list)
+    print(_q_list)
     # show_grid_nparticles(grid, _q_list)
 
-    force4cnn = torch.zeros((nparticle, DIM))
+    k_nearest_nparticle_app = []
+    k_nearest_coord_nparticle_app = []
 
     for i in range(nparticle):
 
-        print(i)
+        print(_q_list[:,i].shape)
+        print(_q_list[:,i])
 
-        k_nearest_up_left = torch.floor(_q_list[i] / grid_interval ) * grid_interval
-        k_nearest_up_left_x = k_nearest_up_left[0]
-        k_nearest_up_left_y = k_nearest_up_left[1]
+        k_nearest = k_nearest_grids(_q_list[:,i], grid_interval)
+        print('k nearest', k_nearest)
+        print(k_nearest.shape)
 
-        k_nearest_up_right = torch.tensor([k_nearest_up_left_x, k_nearest_up_left_y + grid_interval])
-        k_nearest_down_left = torch.tensor([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y])
-        k_nearest_down_right = torch.tensor([k_nearest_up_left_x + grid_interval, k_nearest_up_left_y + grid_interval])
+        k_nearest_up_left_distance    = euclidean_distance(_q_list[:, i], k_nearest[:,0])
+        k_nearest_up_right_distance   = euclidean_distance(_q_list[:, i], k_nearest[:,1])
+        k_nearest_down_left_distance  = euclidean_distance(_q_list[:, i], k_nearest[:,2])
+        k_nearest_down_right_distance = euclidean_distance(_q_list[:, i], k_nearest[:,3])
 
-        k_nearest = torch.stack([k_nearest_up_left, k_nearest_up_right, k_nearest_down_left, k_nearest_down_right])
-        print(k_nearest)
+        print(k_nearest_up_left_distance, k_nearest_up_right_distance, k_nearest_down_left_distance, k_nearest_down_right_distance)
 
-        k_nearest_coord = ( boxsize / 2. + k_nearest ) / grid_interval
-        kind = torch.round(k_nearest_coord).long()
+        k_nearest_distance_cat = torch.stack((k_nearest_up_left_distance, k_nearest_up_right_distance, k_nearest_down_left_distance, k_nearest_down_right_distance),dim=-1)
+        k_nearest_nparticle_app.append(k_nearest_distance_cat)
 
+        kind = k_nearest_coord(k_nearest, grid_interval)
+        k_nearest_coord_nparticle_app.append(kind)
 
+    k_nearest_distance_nparticle = torch.stack(k_nearest_nparticle_app, dim=1) #nsample x nparticle x k_nearest x DIM
+    k_nearest_coord_nparticle = torch.stack(k_nearest_coord_nparticle_app, dim=1)
+
+    print('distance cat', k_nearest_distance_nparticle)
+    print(k_nearest_distance_nparticle.shape)
+    print(k_nearest_coord_nparticle)
+    print(k_nearest_coord_nparticle.shape)
+
+    print(U.shape)
+    # print(k_nearest_coord_nparticle[:,:,:,0],  k_nearest_coord_nparticle[:,:,:,1])
+    # print(k_nearest_coord_nparticle[:,:,:,0].shape,  k_nearest_coord_nparticle[:,:,:,1].shape)
+
+    predict_app = []
+
+    for z in range(nsamples):
+
+        print('z', z)
+        predict_up_left = U[z, :, k_nearest_coord_nparticle[z,:,0,0], k_nearest_coord_nparticle[z,:,0,1]]
+        predict_up_right = U[z, :, k_nearest_coord_nparticle[z,:,1,0], k_nearest_coord_nparticle[z,:,1,1]]
+        predict_down_left = U[z, :, k_nearest_coord_nparticle[z,:,2,0], k_nearest_coord_nparticle[z,:,2,1]]
+        predict_down_right = U[z, :, k_nearest_coord_nparticle[z,:,3,0], k_nearest_coord_nparticle[z,:,3,1]]
+
+        print(predict_up_left.shape, predict_up_right.shape, predict_down_left.shape, predict_down_right.shape)
+        print(predict_up_left, predict_up_right, predict_down_left, predict_down_right)
+        predict_cat = torch.stack((predict_up_left, predict_up_right, predict_down_left, predict_down_right), dim=-1)
+        print('predict_cat', predict_cat.shape)
+        predict_app.append(predict_cat)
+
+    predict_k_nearest_force = torch.stack(predict_app)  # sample x npartilce x  DIM x k_nearest
+
+    print(predict_k_nearest_force)
+    print(predict_k_nearest_force.shape)
+
+    z_l = torch.sum(1. / k_nearest_distance_nparticle, dim=-1)
+    z_l = z_l.unsqueeze(dim=1)
+    print('z_l', z_l)
+    print(z_l.shape)
+
+    k_nearest_distance_nparticle = k_nearest_distance_nparticle.unsqueeze(dim = 1)
+    print(k_nearest_distance_nparticle)
+    print(k_nearest_distance_nparticle.shape)
+
+    print(k_nearest_distance_nparticle * predict_k_nearest_force)
+    print((k_nearest_distance_nparticle * predict_k_nearest_force ).shape)
+    print(torch.sum(k_nearest_distance_nparticle * predict_k_nearest_force, dim=-1))
+    print(z_l * torch.sum(k_nearest_distance_nparticle * predict_k_nearest_force, dim=-1))
+
+    predict_each_particle = 1./z_l * (torch.sum(1./k_nearest_distance_nparticle * predict_k_nearest_force, dim=-1))
+    print(predict_each_particle)
+    print(predict_each_particle.shape)
