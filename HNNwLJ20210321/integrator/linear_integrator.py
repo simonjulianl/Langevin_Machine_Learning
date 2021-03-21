7f3fcff91d1a7faf1c89734f7e3fc4917b0fc780
spark <ssojeongpark@gmail.com>
2021-03-21 12:53:12 +0800

    avoid duplicate

--- HNNwLJ20210321/integrator/linear_integrator.py
+++ HNNwLJ20210321/integrator/linear_integrator.py
@@ -22,6 +22,9 @@ class linear_integrator:
         assert (linear_integrator._obj_count == 1),type(self).__name__ + " has more than one object"
 
         self._integrator_method = integrator_method
+        self.nparticle = MC_parameters.nparticle
+        self.boxsize = MC_parameters.boxsize
+        self.iteration_batch = MD_parameters.iteration_batch
 
     def save_object(self, qp_list, filename, nfile):
         with gzip.open( filename + '_{}.pt'.format(nfile), 'wb') as handle: # overwrites any existing file
@@ -34,49 +37,14 @@ class linear_integrator:
 
             return p
 
+    # to find gold standard
     def step(self, hamiltonian, phase_space, MD_iterations, nsamples_cur, tau_cur):
 
-        boxsize =  MC_parameters.boxsize
-
-        q_list = None
-        p_list = None
-
-        # for i in trange(self._state['MD_iterations']):
-        for i in range(MD_iterations):
-            # print(i)
-            q_curr, p_curr  = self._integrator_method( hamiltonian, phase_space, tau_cur, boxsize)
-            q_curr = torch.unsqueeze(q_curr, dim=0)
-            p_curr = torch.unsqueeze(p_curr, dim=0)
-
-            if i == 0:
-                q_list = q_curr
-                p_list = p_curr
-            else:
-                q_list = torch.cat((q_list, q_curr))
-                p_list = torch.cat((p_list, p_curr))
-
-            assert q_list.shape == p_list.shape
-
-        # q_list = q_list[-1]; p_list = p_list[-1]  # only take the last from the list
-
-        if (torch.isnan(q_list).any()) or (torch.isnan(p_list).any()):
-            # index = torch.where(torch.isnan(q_list))
-            # print(q_list[index])
-            print('Numerical Integration error, nan is detected')
-            # raise ArithmeticError('Numerical Integration error, nan is detected')
-
-        return (q_list, p_list)
-
-    # to find gold standard
-    def tiny_step(self, hamiltonian, phase_space, MD_iterations, nsamples_cur, tau_cur, filename):
-
-        nparticle = MC_parameters.nparticle
-        boxsize =  MC_parameters.boxsize
-        iteration_batch = MD_parameters.iteration_batch
-        iteration_pair_batch = iteration_batch * int(MD_parameters.tau_long / tau_cur)
+        iteration_pair_batch = self.iteration_batch * int(MD_parameters.tau_long / tau_cur)
+        filename = 'tmp/nparticle{}_tau{}'.format(self.nparticle, tau_cur)
 
         print('step nsamples_cur, tau_cur, MD_iterations, iteration_batch ')
-        print(nsamples_cur, tau_cur, MD_iterations, iteration_batch)
+        print(nsamples_cur, tau_cur, MD_iterations, self.iteration_batch)
 
         qp_list = []
 
@@ -84,7 +52,7 @@ class linear_integrator:
 
             #print('iteration', i)
 
-            q_list_, p_list_  = self._integrator_method(hamiltonian, phase_space, tau_cur, boxsize)
+            q_list_, p_list_  = self._integrator_method(hamiltonian, phase_space, tau_cur, self.boxsize)
 
             qp_stack = torch.stack((q_list_, p_list_))
             # qp = {'q_{}'.format(i): q_list, 'p_{}'.format(i): p_list}
@@ -92,7 +60,6 @@ class linear_integrator:
             if (i+1) % int(MD_parameters.tau_long / tau_cur) == 0 :
                 qp_list.append(qp_stack)
                 print('i', i, 'memory % used:', psutil.virtual_memory()[2])
-
 
             if i % iteration_pair_batch == iteration_pair_batch - 1:
 
@@ -103,12 +70,12 @@ class linear_integrator:
                 qp_list = []
 
 
-    def concat_tiny_step(self, MD_iterations, tau_cur, filename):
+    def concat_step(self, MD_iterations, tau_cur):
 
          qp_list = []
 
-         iteration_batch = MD_parameters.iteration_batch
-         iteration_pair_batch = iteration_batch * int(MD_parameters.tau_long / tau_cur)
+         iteration_pair_batch = self.iteration_batch * int(MD_parameters.tau_long / tau_cur)
+         filename = 'tmp/nparticle{}_tau{}'.format(self.nparticle, tau_cur)
          print('iteration pair batch', iteration_pair_batch)
 
          nfile = int(MD_iterations / iteration_pair_batch )
@@ -131,7 +98,6 @@ class linear_integrator:
              qp_list.append(qp_stack)
              #print('memory % used:', psutil.virtual_memory()[2])
 
-
          qp_cat_list = torch.cat(qp_list, dim=1)
 
          q_list = qp_cat_list[0]
@@ -143,32 +109,3 @@ class linear_integrator:
              raise ArithmeticError('Numerical Integration error, nan is detected')
 
          return (q_list, p_list)
-
-
-    # def load_nfile_tiny_step(self, MD_iterations, filename):
-    #
-    #     self.qp_cat_list = []
-    #     iteration_batch = MD_parameters.iteration_batch
-    #     print('iteration batch', iteration_batch)
-    #     nfile = int( MD_iterations / MD_parameters.iteration_batch )
-    #
-    #     for j in range(nfile):
-    #         print('j',j)
-    #
-    #         #a = self.load_object(filename, j)
-    #         with open( filename + '_{}.pt'.format(j), 'rb') as handle: # overwrites any existing file
-    #             a = pickle.load(handle)
-    #
-    #         handle.close()
-    #         print('j',j, a)
-    #         self.qp_cat_list.append(a)
-    #         print('list',self.qp_cat_list)
-    #
-    #         if j % iteration_batch == iteration_batch - 1:
-    #
-    #
-    #             a.clear()
-    #
-    #         print('memory % used:', psutil.virtual_memory()[2])
-    #
-    #     return self.qp_cat_list
