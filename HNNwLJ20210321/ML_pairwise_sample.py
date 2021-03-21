f96af4afa504b5a465d6574c2cff370ede3fe60c
spark <ssojeongpark@gmail.com>
2021-03-21 15:24:40 +0800

    ML main

--- HNNwLJ20210321/ML_pairwise_sample.py
+++ HNNwLJ20210321/ML_pairwise_sample.py
@@ -7,7 +7,6 @@ from HNN.models import pair_wise_MLP
 # from HNN.models import pair_wise_zero
 from HNN.MD_learner import MD_learner
 from HNN.MD_tester import MD_tester
-from HNN.MD_crash_relearner import MD_crash_relearner
 from parameters.MC_parameters import MC_parameters
 from parameters.MD_parameters import MD_parameters
 from parameters.ML_parameters import ML_parameters
@@ -55,22 +54,22 @@ if not os.path.exists('./tmp/'):
 load_path = './saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_checkpoint.pth'.format( nsamples, nparticle, tau_long, optimizer,
                                                  lr, MLP_nhidden, activation)
 
-# # train
-# best_model_path = './saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_checkpoint_best.pth'.format( nsamples, nparticle, tau_long, optimizer,
-#                                                      lr, MLP_nhidden, activation)
-# save_path = './saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_checkpoint.pth'.format( nsamples, nparticle, tau_long, optimizer,
-#                                                  lr, MLP_nhidden, activation)
-# loss_curve = 'nsamples{}_nparticle{}_tau{}_{}_{}_lr{}_h{}_{}_loss.txt'.format(nsamples, nparticle,  tau_long, tau_short, optimizer, lr, MLP_nhidden, activation)
+# train
+best_model_path = './saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_checkpoint_best.pth'.format( nsamples, nparticle, tau_long, optimizer,
+                                                     lr, MLP_nhidden, activation)
+save_path = './saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_checkpoint.pth'.format( nsamples, nparticle, tau_long, optimizer,
+                                                 lr, MLP_nhidden, activation)
+loss_curve = 'nsamples{}_nparticle{}_tau{}_{}_{}_lr{}_h{}_{}_loss.txt'.format(nsamples, nparticle,  tau_long, tau_short, optimizer, lr, MLP_nhidden, activation)
 
 
-# when retrain
-best_model_path = './retrain_saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_crash_{}_checkpoint_best.pth'.format( nsamples, nparticle, tau_long, optimizer,
-                                                     lr, MLP_nhidden, activation, crash_duplicate_ratio)
-# when retrain
-save_path = './retrain_saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_crash_{}_checkpoint.pth'.format( nsamples, nparticle, tau_long, optimizer,
-                                                 lr, MLP_nhidden, activation, crash_duplicate_ratio)
-# when retrain
-loss_curve = 'nsamples{}_nparticle{}_tau{}_{}_{}_lr{}_h{}_{}_crash_{}_loss.txt'.format(nsamples, nparticle,  tau_long, tau_short, optimizer, lr, MLP_nhidden, activation, crash_duplicate_ratio)
+# # when retrain
+# best_model_path = './retrain_saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_crash_{}_checkpoint_best.pth'.format( nsamples, nparticle, tau_long, optimizer,
+#                                                      lr, MLP_nhidden, activation, crash_duplicate_ratio)
+# # when retrain
+# save_path = './retrain_saved_model/nsamples{}_nparticle{}_tau{}_{}_lr{}_h{}_{}_crash_{}_checkpoint.pth'.format( nsamples, nparticle, tau_long, optimizer,
+#                                                  lr, MLP_nhidden, activation, crash_duplicate_ratio)
+# # when retrain
+# loss_curve = 'nsamples{}_nparticle{}_tau{}_{}_{}_lr{}_h{}_{}_crash_{}_loss.txt'.format(nsamples, nparticle,  tau_long, tau_short, optimizer, lr, MLP_nhidden, activation, crash_duplicate_ratio)
 
 uppath = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
 base_dir = uppath(__file__, 1)
@@ -82,8 +81,8 @@ torch.autograd.set_detect_anomaly(True)
 
 # # for train
 MD_learner = MD_learner(linear_integrator_obj, pair_wise_HNN_obj, phase_space, init_path, crash = None)
-# MD_learner.load_checkpoint(load_path)
-# MD_learner.train_valid_epoch(save_path, best_model_path, loss_curve)
+MD_learner.load_checkpoint(load_path)
+MD_learner.train_valid_epoch(save_path, best_model_path, loss_curve)
 
 # # for test
 # start = time.time()
