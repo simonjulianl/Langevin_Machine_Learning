fef3f8d4feb9ed9039c0129fd436968a6dc4afaa
spark <ssojeongpark@gmail.com>
2021-03-24 12:17:00 +0800

    clean code

--- HNNwLJ20210321/parameters/MD_parameters.py
+++ HNNwLJ20210321/parameters/MD_parameters.py
@@ -3,21 +3,25 @@ import math
 
 class MD_parameters:
 
-    seed = 4662570 # index for preparing data
+    # seed = 4662570 # index for preparing data
     nsamples = 6 # total train/valid 20900 / for predict 20900 / for inegrate 1000
-    nsamples_batch = 10
-    pixels_batch = 32
+    nsamples_batch = 10  # num. of batch for nsamples when prepare data before ML
     nsamples_ML = 1
     epsilon = 1.
     sigma = 1.
     mass = 1
     temp_list = [0.04]
-    npixels = 32
     tau_short = 0.001  # short time step for label
     tau_long = 0.1
-    max_ts = 10.
-    # iteration_batch = 10
-    iteration_batch = 1   #  :train/valid,  int(max_ts ) : test
-    ML_iteration_batch = 2
-    crash_duplicate_ratio = 0.4
+    nstack = 1  # 100 th step at short time step paired with first large time step
+    tau_pair = int(tau_long / tau_short) # n iterations of short time step paired w large time step
+    max_ts = 10. # use for predict more iterations
+    iteration_batch = 1   #  use linear_integrator; setting 1 : train/valid,  int(max_ts ) : test
+
+    crash_duplicate_ratio = 0.4 # use data_io
     integrator_method = methods.linear_velocity_verlet
+
+    # optical flow
+    pixels_batch = 32
+    npixels = 32
+    # ML_iteration_batch = 2
--- HNNwLJ20210321/parameters/ML_parameters.py
+++ HNNwLJ20210321/parameters/ML_parameters.py
@@ -1,10 +1,26 @@
 import torch
+import torch.optim as optim
+
+class optimizer:
+
+    def __init__(self, lr):
+
+        self.lr = lr
+        self.opttype = optim.Adam
+        self.optname = self.opttype.__name__
+
+    def create(self, para):
+        return self.opttype(para, self.lr)
+
+    def name(self):
+        return self.optname
 
 class ML_parameters:
 
+    seed = 9372211
     lr = 0.0001
     nepoch = 5
-    optimizer = 'Adam'
+    opt = optimizer(lr)
     activation = 'tanh'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
